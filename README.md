8ea7295e7245431f12d0ea10086d294568f89e14
bmartinc80 <bmartinc80@gmail.com>
2023-10-11 09:17:40 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# nlp-news-classification
+# NEWS CLASIFFICATION 🦅
+
+<p align="center">
+    <img src="images/sample_image.jpg" width="500" height="400"/>
+</p>
+
+This repository hosts a notebook featuring an in-depth analysis of several **RNN** models, together with **CNN** and **Multinomial Naive Bayes** along with an app deployment using Streamlit. The following models were meticulously evaluated:
+
+- Basic Keras Model
+- LSTM Model
+- LSTM GRU Model
+- LSTM Bidirectional Model
+    - TextVectorization + Keras Embedding
+    - Text_to_word_sequence	+ Word2Vec Embedding
+- Basic CNN Model
+
+
+
+The dataset used has been downloaded from [Kaggle](https://www.kaggle.com/datasets/gpiosenka/100-bird-species) and contains a set of 525 bird species. 84635 training images, 2625 test images(5 images per species) and 2625 validation images(5 images per species).
+
+The app can be tested in **Hugging Face** (.py files hosted there) following this [link](https://huggingface.co/spaces/bmartinc80/birds_pytorch).
+
+## 👨‍💻 **Tech Stack**
+
+
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+![Jupyter Notebook](https://img.shields.io/badge/jupyter-%23FA0F00.svg?style=for-the-badge&logo=jupyter&logoColor=white)
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![Pandas](https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white)
+![NumPy](https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white)
+![PyTorch](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white)
+![Matplotlib](https://img.shields.io/badge/Matplotlib-%23d9ead3.svg?style=for-the-badge&logo=Matplotlib&logoColor=black)
+![Linux](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black)
+![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+
+
+## 🪶 Set Up
+
+In the first stage, a set of helper functions was created in order to easily perform the modelling and prediction
+
+- **Set seed**: Set random seeds for PyTorch operations, both on the CPU and the GPU
+- **DataLoaders**: Create data loaders for training and testing datasets using PyTorch's DataLoader class
+- **Writer**: SummaryWriter object for logging experiments and metrics in TensorBoard
+- **Training and Testing**: Several functions for training and testing a PyTorch model 
+- **Plots**: Several plots including loss curve, predictions and images
+
+## 📳 Modelling
+
+The first approach was to train 2 Pytorch EfficientNet models (EffNetB0, EffNetB2) with **5 and 10 epochs** using the pretrained model weights of EffNetB0 for the DataLoaders in order to stablish a baseline. The **EffNetB2 with 10 epochs** showed the best performance above **93%** on the test set.
+
+<p align="center">
+    <img src="images/accuracy.png" width="700" height="500"/>
+</p>
+
+
+<p align="center">
+    <img src="images/birds_predictions.png"/>
+</p>
+
+## ↗️ Model Improvement
+
+Then the EffNetB2 with 10 epochs was trained again but this time using the pretrained model weights of EffNetB2 for the DataLoaders. This time an accuracy above **95%** on the **test set** and above **93%** on the **validation set** was achieved .
+
+<p align="center">
+    <img src="images/effnetB2.png"/>
+</p>
+
+## 👏 App Deployment
+
+The last step was to deploy and app hosted in Hugging Face using Gradio. This app can be tested with available sample images or with own ones.
+
+<p align="center">
+    <img src="images/app_deployment.png"/>
+</p>
---
+++ app/app.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import streamlit as st
+import tensorflow as tf
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+# Text Preprocessing
+def preprocess_text(text_series):
+    # Initialize an empty list to store the preprocessed texts
+    preprocessed_texts = []
+
+    for text in text_series:
+        # Remove punctuation
+        for punctuation in string.punctuation:
+            text = text.replace(punctuation, ' ')
+
+        # Lowercase the text
+        text = text.lower()
+
+        # Tokenize the text
+        tokenized = word_tokenize(text)
+
+        # Remove non-alphabetic words
+        words_only = [word for word in tokenized if word.isalpha()]
+
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        without_stopwords = [word for word in words_only if word not in stop_words]
+
+        # Lemmatize the words
+        lemma = WordNetLemmatizer()
+        lemmatized = [lemma.lemmatize(word) for word in without_stopwords]
+
+        # Join the cleaned words back into a string
+        cleaned_text = ' '.join(lemmatized)
+
+        # Append the preprocessed text to the list
+        preprocessed_texts.append(cleaned_text)
+
+    return preprocessed_texts
+
+# Prediction
+def predict_fake_news(text, model):
+    # Preprocess the text
+    cleaned_text = preprocess_text(text)
+
+    # Make predictions
+    output = model.signatures['serving_default'](input_5=tf.constant([cleaned_text]))
+
+
+    # Extract the output (you may need to adapt this based on your model's output)
+    # For example, if your model outputs a single probability, you can do:
+    fake_news_probability = output['dense_4'].numpy()[0]  # Replace 'dense_4' with the correct output name
+
+    return fake_news_probability
+
+def main():
+    """ NLP Based App with Streamlit """
+
+    # Set page title and configure layout
+    st.set_page_config(page_title="Fake News Detection App", page_icon="📰", layout="wide")
+
+    # Add a title and description
+    st.title("Fake News Detection App")
+    st.markdown("""
+        #### Description
+        The news detection uses the best trained model, a TensorFlow Bidirectional LSTM Model, which achieved an accuracy above 99.84 %. You can find the notebook with the different trained models in the following [repository](https://github.com/benitomartin/nlp-news-classification)
+        """)
+
+    # Add a heading for user input
+    st.header("Input News Text")
+
+    # Create a text area for user input
+    message = st.text_area("🗞️🗞️🗞️ Enter the news text 🗞️🗞️🗞️", placeholder="Paste the news text here...")
+
+    text_series = pd.Series([message])
+
+    
+    # Load your TensorFlow SavedModel
+    model = tf.saved_model.load('C:/Users/bmart/OneDrive/15_NLP/nlp-news-classification/model')  # Replace 'your_model_directory' with the actual directory containing your SavedModel
+
+
+    if st.button("Predict"):
+        fake_news_probability = predict_fake_news(text_series, model)
+
+        # Determine whether it's True or Fake based on the threshold (0.5)
+        result = "True" if fake_news_probability > 0.5 else "Fake"
+
+        # Display the result in a styled div element
+        if result == "True":
+            st.success("This news is labeled as:")
+        else:
+            st.error("This news is labeled as:")
+
+        # Display the result with a color-coded label
+        st.write(f"<div style='font-size: 24px; text-align: center; color: {'green' if result == 'True' else 'red'};'>{result}</div>", unsafe_allow_html=True)
+
+    st.sidebar.subheader("About the App")
+    st.sidebar.info("This App is based on the following [Kaggle](https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset/data) dataset")
+    st.sidebar.markdown("![Image](https://cdn.britannica.com/25/93825-050-D1300547/collection-newspapers.jpg)")
+
+
+if __name__ == '__main__':
+    main()
